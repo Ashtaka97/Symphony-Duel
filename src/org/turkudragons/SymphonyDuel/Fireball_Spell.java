a0b31e53134594e130570931ec53f85fee8b178c
Ashtaka97 <tommihheikkinen@gmail.com>
2018-06-01 19:35:48 +0300

    info

--- src/org/turkudragons/SymphonyDuel/Fireball_Spell.java
+++ src/org/turkudragons/SymphonyDuel/Fireball_Spell.java
@@ -1,9 +1,11 @@
 package org.turkudragons.SymphonyDuel;
 
+import org.newdawn.slick.geom.Shape;
+
 public class Fireball_Spell extends Spell {
 
 	public Fireball_Spell() {
-		//int power, int count, boolean aoe, Type type, Element element, float defx, float defy)
+		//(int power, int count, boolean collidable, boolean aoe, Shape area, Type type, Element element, float defx, float defy, float speed, String chant)
 		super(10, 1, true, false, null, Type.ATTACK, Element.HEAT,((Player)LocalPvP.oList.get(0)).x,((Player)LocalPvP.oList.get(0)).y, 1, "DDD");		
 	}
 	
