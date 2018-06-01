e2a129dccbbe76468c1160d7da53a551f54bf967
Ashtaka97 <tommihheikkinen@gmail.com>
2018-06-01 17:43:48 +0300

    Updated spell and player

--- src/org/turkudragons/SymphonyDuel/Fireball_Spell.java
+++ src/org/turkudragons/SymphonyDuel/Fireball_Spell.java
@@ -5,7 +5,7 @@ public class Fireball_Spell extends Spell {
 	
 	public Fireball_Spell() {
 		//int power, int count, boolean aoe, Type type, Element element, float defx, float defy)
-		super(10, 1, false, Type.ATTACK, Element.HEAT);
+		super(10, 1, false, Type.ATTACK, Element.HEAT,((Player)LocalPvP.oList.get(0)).x,((Player)LocalPvP.oList.get(0)).y);
 		
 	}
 	
--- src/org/turkudragons/SymphonyDuel/Player.java
+++ src/org/turkudragons/SymphonyDuel/Player.java
@@ -17,5 +17,5 @@ public class Player {
 	public void update() {
 		
 	}
-	
+
 }
