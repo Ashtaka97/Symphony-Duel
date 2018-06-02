60d02eaf97ff1192d649d2d3469a4eed33ddb6c1
Santtinen <santtinen3@gmail.com>
2018-06-02 15:37:14 +0300

    Rythym pulse

--- src/org/turkudragons/SymphonyDuel/LocalPvP.java
+++ src/org/turkudragons/SymphonyDuel/LocalPvP.java
@@ -50,95 +50,193 @@ public class LocalPvP extends BasicGameState implements GameState {
 
 	private int timer;
 	int tapInterval;
+	private int timer2;
+	int tapInterval2;
+	private int step;
+	private int grace;
+	private int grace2;
 	private Input input;
 	private Player p1;
 	private Player p2;
 	private String chantP1;
 	private String chantP2;
-	static ArrayList<Object> oList;
+	static ArrayList<Player> oList;
 	private ArrayList<Shape> timerList;
+	private ArrayList<Shape> timerList2;
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		timer = 0;
-		tapInterval = 500;
+		tapInterval = 750;
+		timer2 = 0;
+		tapInterval2 = 750;
+		step = 1;
+		grace = 0;
+		grace2 = 0;
 		input = gc.getInput();
 		chantP1 = "";
 		chantP2 = "";
-		p1 = new Player(200, 200);
-		p2 = new Player(1000, 200);
-		oList = new ArrayList<Object>();
+		p1 = new Player(200, 500);
+		p2 = new Player(1000, 500);
+		oList = new ArrayList<Player>();
 		oList.add(p1);
 		oList.add(p2);
 		timerList = new ArrayList<Shape>();
-		timerList.add(new Rectangle(200, 100, 10, 20));
+		timerList.add(new Rectangle(100, 100, 50, 20));
+		timerList.add(new Rectangle(85, 100, 15, 20));
+		timerList2 = new ArrayList<Shape>();
+		timerList2.add(new Rectangle(800, 100, 50, 20));
+		timerList2.add(new Rectangle(785, 100, 15, 20));
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		g.drawString(timer+"", 550, 100);
-		for(Object o : oList) {
-			
+		g.drawString(timer2+"", 600, 100);
+		for(Player p : oList) {
+			p.draw(g);
 		}
+		g.drawString(chantP1, 200, 450);
+		g.drawString(chantP2, 1000, 450);
 		for(Shape s : timerList) {
+			g.draw(s);
+		}
+		for(Shape s : timerList2) {
 			g.draw(s);
 		}
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-
+		
+		try {
+			
+			// Player 1 chanting
+			if(grace <= 0) {
+				if (input.isKeyDown(Input.KEY_W)) {
+					if(timerList.get(0).intersects(timerList.get(2))) chantP1 += "W";
+					else chantP1 = "";
+					grace = 300;
+					timerList.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_S)) {
+					if(timerList.get(0).intersects(timerList.get(2))) chantP1 += "S";
+					else chantP1 = "";
+					grace = 300;
+					timerList.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_A)) {
+					if(timerList.get(0).intersects(timerList.get(2))) chantP1 += "A";
+					else chantP1 = "";
+					grace = 300;
+					timerList.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_D)) {
+					if(timerList.get(0).intersects(timerList.get(2))) chantP1 += "D";
+					else chantP1 = "";
+					grace = 300;
+					timerList.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_SPACE)) {
+					checkSpell(chantP1);
+					chantP1 = "";
+				}
+			}
+			
+			// Player 2 chanting
+			if(grace2 <= 0) {
+				if (input.isKeyDown(Input.KEY_UP)) {
+					if(timerList2.get(0).intersects(timerList2.get(2))) chantP2 += "W";
+					else chantP2 = "";
+					grace2 = 300;
+					timerList2.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_DOWN)) {
+					if(timerList2.get(0).intersects(timerList2.get(2))) chantP2 += "S";
+					else chantP2 = "";
+					grace2 = 300;
+					timerList2.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_LEFT)) {
+					if(timerList2.get(0).intersects(timerList2.get(2))) chantP2 += "A";
+					else chantP2 = "";
+					grace2 = 300;
+					timerList2.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_RIGHT)) {
+					if(timerList2.get(0).intersects(timerList2.get(2))) chantP2 += "D";
+					else chantP2 = "";
+					grace2 = 300;
+					timerList2.remove(2);
+				}
+				else if (input.isKeyDown(Input.KEY_ENTER)) {
+					checkSpell(chantP2);
+					chantP2 = "";
+				}
+			}
+		} catch(IndexOutOfBoundsException e) {
+			
+		}
+		
+		if(step == 1) {
+			int i = 0;
+			for(Shape s : timerList) {
+				if(i < 2) i++;
+				else{
+					s.setX(s.getX()-delta);
+				}
+			}
+			int i2 = 0;
+			for(Shape s : timerList2) {
+				if(i2 < 2) i2++;
+				else{
+					s.setX(s.getX()-delta);
+				}
+			}
+			step = 0;
+		}
+		else step++;
+		
 		if(timer >= tapInterval) {
-			timerList.add(new Rectangle(3, 20, 0, 0));
+			timerList.add(new Rectangle(500, 100, 3, 20));
 			timer = 0;
 		}
 		else timer += 1+delta;
 		
+		if(timer2 >= tapInterval2) {
+			timerList2.add(new Rectangle(1200, 100, 3, 20));
+			timer2 = 0;
+		}
+		else timer2 += 1+delta;
 		
+		try {
+			if(timerList.get(2).intersects(timerList.get(1))) {
+				timerList.remove(2);
+				chantP1 = "";
+				grace = 300;
+			}
+		} catch(IndexOutOfBoundsException e) {
+			
+		}
 		
-		// Player 1 chanting
-		if (input.isKeyPressed(Input.KEY_W)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP1 += "W";
-			else chantP1 = "";
+		if(grace > 0) {
+			grace -= delta;
 		}
-		else if (input.isKeyPressed(Input.KEY_S)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP1 += "S";
-			else chantP1 = "";
+		
+		try {
+			if(timerList2.get(2).intersects(timerList2.get(1))) {
+				timerList2.remove(2);
+				chantP2 = "";
+				grace2 = 300;
+			}
+		} catch(IndexOutOfBoundsException e) {
+			
 		}
-		else if (input.isKeyPressed(Input.KEY_A)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP1 += "A";
-			else chantP1 = "";
+		
+		if(grace2 > 0) {
+			grace2 -= delta;
 		}
-		else if (input.isKeyPressed(Input.KEY_D)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP1 += "D";
-			else chantP1 = "";
-		}
-		else if (input.isKeyPressed(Input.KEY_SPACE)) {
-			checkSpell(chantP1);
-			chantP1 = "";
-		}
-
-		// Player 2 chanting
-		if (input.isKeyPressed(Input.KEY_UP)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP2 += "W";
-			else chantP2 = "";
-		}
-		else if (input.isKeyPressed(Input.KEY_DOWN)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP2 += "S";
-			else chantP2 = "";
-		}
-		else if (input.isKeyPressed(Input.KEY_LEFT)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP2 += "A";
-			else chantP2 = "";
-		}
-		else if (input.isKeyPressed(Input.KEY_RIGHT)) {
-			if(timerList.get(0).intersects(timerList.get(1))) chantP2 += "D";
-			else chantP2 = "";
-		}
-		else if (input.isKeyPressed(Input.KEY_ENTER)) {
-			checkSpell(chantP2);
-			chantP2 = "";
-		}
+		
 	}
 
 	private void checkSpell(String s) {
--- src/org/turkudragons/SymphonyDuel/Player.java
+++ src/org/turkudragons/SymphonyDuel/Player.java
@@ -1,17 +1,27 @@
 package org.turkudragons.SymphonyDuel;
 
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
 public class Player {
 
 	int hp;
 	int x;
 	int y;
+	Shape hitbox;
 	double castSpeed;
 	
 	public Player(int x, int y) {
 		hp = 100;
 		this.x = x;
 		this.y = y;
+		hitbox = new Rectangle(x, y, 16, 32);
 		castSpeed = 1.0;
+	}
+	
+	public void draw(Graphics g) {
+		g.draw(hitbox);
 	}
 	
 	public void update() {
