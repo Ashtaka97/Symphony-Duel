643cb8f42596a72aeee861ab36ac01f10ed0f553
Santtinen <santtinen3@gmail.com>
2018-09-07 23:02:58 +0300

    Fixed stuff

--- src/org/turkudragons/SymphonyDuel/GiftOfLife_Spell.java
+++ src/org/turkudragons/SymphonyDuel/GiftOfLife_Spell.java
@@ -8,7 +8,7 @@ public class GiftOfLife_Spell extends Spell {
 	
 	public GiftOfLife_Spell() {
 		//(int power, int count, boolean collidable, Shape area, Type type, Target target, Element element, float speed, String chant, String name)
-		super(100, 1, false, Type.BUFFDEBUFF, Target.SELF, Element.RADIANT, 1, "WWW", "Gift Of Life");
+		super(50, 1, false, Type.BUFFDEBUFF, Target.SELF, Element.RADIANT, 1, "WWW", "Gift Of Life");
 	}
 	
 	public void cast(Player caster, Player opponent, ArrayList<Object> oList, boolean crit) {
